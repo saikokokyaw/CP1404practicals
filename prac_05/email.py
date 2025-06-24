8164f2adbac5c35f91c10e07e603a9843a6b2937
SKKK <saikokokyaw@my.jcu.edu.au>
2025-06-24 18:23:52 +0630

    week 5 practicals email

---
+++ prac_05/email.py
@@ -0,0 +1,35 @@
+"""
+Word Occurrences
+Estimate: 30 minutes
+Actual:   42 minutes
+"""
+
+"""
+CP1404 Practical - Suggested Solution
+Email to name dictionary
+"""
+def main():
+    """Create dictionary of emails-to-names."""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if confirmation.upper() != "Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
+    """Extract expected name from email address."""
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
